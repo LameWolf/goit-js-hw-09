957540282e376b675c1244c902df8773fd802242
LameWolf <yaroslavtkach@outlook.com>
2023-05-07 20:08:11 +0000

    Deploying to gh-pages from @ LameWolf/goit-js-hw-09@101e3c06f89cfa2386ec734389e9e65ee4c56b3d 🚀

---
+++ 01-color-switcher.c3353007.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),timerId:null};t.btnStart.addEventListener("click",(()=>{t.timerId=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t,console.log(t)}),1e3)})),t.btnStop.addEventListener("click",(()=>{clearInterval(t.timerId),console.log("TimerId has stopped")}));
+//# sourceMappingURL=01-color-switcher.c3353007.js.map
