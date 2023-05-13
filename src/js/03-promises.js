87c77eddbfb69535c13e0945eb8c5a6808bd080d
Yaroslav Tkach <yaroslavtkach@outlook.com>
2023-05-13 21:13:48 +0300

    Completed Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+const onFormSubmit = evt => {
+  evt.preventDefault();
+
+  let delayValue = Number(evt.currentTarget.delay.value);
+  let stepValue = Number(evt.currentTarget.step.value);
+  let amount = Number(evt.currentTarget.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayValue += stepValue;
   }
-}
+  form.reset();
+};
+
+const createPromise = (position, delay) => {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+form.addEventListener('submit', onFormSubmit);
